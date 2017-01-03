66da141c619dd7f9f5375405e6d316683514d939
ShiKaiWi <xykwei@gmail.com>
2017-01-03 17:04:08 +0800

    add slim ProgressBar when requesting the article

--- app/src/main/java/com/example/xkwei/gankio/widgets/ArticlePageFragment.java
+++ app/src/main/java/com/example/xkwei/gankio/widgets/ArticlePageFragment.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.xkwei.gankio.R;
 
@@ -21,6 +24,7 @@ public class ArticlePageFragment extends Fragment {
     private static final String ARG_URI="Article_Page_Fragment_Article_Uri";
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
     public static ArticlePageFragment newInstance(Uri uri){
         Bundle args = new Bundle();
         args.putParcelable(ARG_URI,uri);
@@ -31,6 +35,7 @@ public class ArticlePageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
         mUri = getArguments().getParcelable(ARG_URI);
     }
 
@@ -38,23 +43,30 @@ public class ArticlePageFragment extends Fragment {
     public View onCreateView(LayoutInflater lif, ViewGroup vg,Bundle savedInstanceState){
         View v = lif.inflate(R.layout.fragment_article_page,vg,false);
         mWebView = (WebView) v.findViewById(R.id.article_page_fragment_webview);
+        mProgressBar = (ProgressBar) v.findViewById(R.id.article_page_fragment_progressbar);
+
         mWebView.getSettings().setJavaScriptEnabled(true);
 
-        mWebView.setWebViewClient(new WebViewClient(){
-            public boolean shouldOverrideUrlLoading(WebView wv,String url){
-                String protocol = url.split(":")[0];
-                if(protocol.equals("https")||protocol.equals("http"))
-                    return false;
+        mProgressBar.setMax(100);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView wv,int newProgress){
+                if(newProgress==100){
+                    mProgressBar.setVisibility(ProgressBar.GONE);
+                }
                 else{
-                    Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
-                    if(i.resolveActivity(getActivity().getPackageManager())!=null) {
-                        startActivity(i);
-                        return true;
-                    }
-                    return false;
+                    mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
                 }
             }
+
+            @Override
+            public void onReceivedTitle(WebView wv,String title){
+                AppCompatActivity activity = (AppCompatActivity)getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
         });
+
         mWebView.loadUrl(mUri.toString());
         return v;
     }
